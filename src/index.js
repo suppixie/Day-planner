36c6cee506818461b7abcebb4ca0162283ce4d8e
suppixie <mamthapatalay6@gmail.com>
2023-12-08 15:55:39 +0530

    Updated the day planner api

--- src/index.js
+++ src/index.js
@@ -5,11 +5,6 @@ import './index.css';
 import Navbar from './components/Navbar';
 import App from './App';
 import DayPlanner from './components/dayplanner/Dayplanner';
-<<<<<<< HEAD
-// import FlightAPI from './components/FlightAPI';
-=======
-
->>>>>>> 7bd196287bbed33d3f946a38425525417d3f35a3
 
 ReactDOM.render(
   <React.StrictMode>
@@ -18,10 +13,6 @@ ReactDOM.render(
       <Routes>
         <Route exact path="/" Component={App} />
         <Route path="/Dayplanner" Component={DayPlanner}/>
-<<<<<<< HEAD
-        {/* <Route path="/trip-planner" Component={FlightAPI} /> */}
-=======
->>>>>>> 7bd196287bbed33d3f946a38425525417d3f35a3
         <Route path="/about-us" Component={App} />
       </Routes>
     </Router>
